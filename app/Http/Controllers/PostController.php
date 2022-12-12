9489e6876c926ec8eaf6dd7fbe107e4d905b9ed8
LizaCastDiaz <lizacastdiaz@gmail.com>
2022-12-12 20:14:14 +0100

    Index is conected to posts. Now in index is gona show the latest post paginated

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
 
-    // queremos que se retornamos la vista llamada 'index' y le pasaremos toda la informacion. Del 'index', Traeme los ultimos posts Post::latest() y me los traes paginados ->paginate().
+    // Es lo que se necesita para hacer mi consulta y que los datos vayan directamente a la vista index
+    // queremos que se retorne la vista llamada 'index'. posts Post::latest() y me los traes paginados ->paginate().
     public function index()
     {
         return view('index', [
